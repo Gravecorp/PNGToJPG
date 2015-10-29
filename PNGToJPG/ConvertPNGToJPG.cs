21e08f311acd5f52f97b34520dba7a57f7ca0ba2
Gravecorp <sakura256@gmail.com>
2015-10-29 19:07:56 +0100

    initial commit

---
+++ PNGToJPG/ConvertPNGToJPG.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNGToJPG
+{
+    public class ConvertPNGToJPG
+    {
+
+        public static void ConvertDir(string directory)
+        {
+            string[] files = Directory.GetFiles(directory, "*.png");
+            foreach (string file in files)
+            {
+                ConvertFile(file);
+            }
+        }
+
+        public static void ConvertFile(string file)
+        {
+            string f = file.Replace(".png", ".jpg");
+            ConvertImage(file, f);
+        }
+
+
+        private static void ConvertImage(string filename, string newFile)
+        {
+            using (Image myImage = Image.FromFile(filename))
+            {
+                using (var b = new Bitmap(myImage.Width, myImage.Height))
+                {
+                    b.SetResolution(myImage.HorizontalResolution, myImage.VerticalResolution);
+
+                    using (var g = Graphics.FromImage(b))
+                    {
+                        g.Clear(Color.White);
+                        g.DrawImageUnscaled(myImage, 0, 0);
+                    }
+
+                    try
+                    {
+                        b.Save(newFile, ImageFormat.Jpeg);
+                    }
+                    catch (Exception)
+                    {
+                        //want it to die silently if it fails to convert.
+                    }
+                    
+                }
+            }
+        }
+    }
+}
---
+++ PNGToJPG/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNGToJPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                string location = Directory.GetCurrentDirectory().ToString();
+                ConvertPNGToJPG.ConvertDir(location);
+            }
+            else if (args.Length == 1)
+            {
+                ConvertPNGToJPG.ConvertFile(args[0]);
+            }
+        }
+    }
+}
